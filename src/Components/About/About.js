db2c421d924bd2bb20bf1f1a20a14f5161598286
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-04-18 22:36:12 +0600

    about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>About</h2>
+            <h2>This is Nahid Ahmeed Likhon</h2>
+            <p>My Goal------- In sha allah I Will Be a Web developer.</p>
         </div>
     );
 };
